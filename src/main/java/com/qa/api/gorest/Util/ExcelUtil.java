4d7457d2f003c58dc0142ddb0e6a039ef3797f32
maaduri <60827635+maaduri@users.noreply.github.com>
2021-04-04 14:39:24 -0700

    Update ExcelUtil.java

--- src/main/java/com/qa/api/gorest/Util/ExcelUtil.java
+++ src/main/java/com/qa/api/gorest/Util/ExcelUtil.java
@@ -27,6 +27,7 @@ public class ExcelUtil {
 		{
 		try{
 		  InputStream ip = ExcelUtil.class.getResourceAsStream("/userdata.xlsx");
+		  System.out.println(ip);
 		  //FileInputStream ip = new FileInputStream("/src/main/resources/userdata.xlsx"); 
 		                              //FileInputstream is used to create a connection with any file. 
 		                              //if file got deleted, file path is wrong. 
